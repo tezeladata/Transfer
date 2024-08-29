93569af3a843ee1c1303809ad52084f1a8a002d0
tezeladata <David.tezela@gmail.com>
2024-08-29 16:02:51 +0400

    commit

--- integer_split.py
+++
@@ -1,9 +0,0 @@
-def split_integer(num, parts):
-    base_size = num // parts 
-    remainder = num % parts  
-    
-    result = [base_size + 1] * remainder + [base_size] * (parts - remainder)
-    
-    return sorted(result)
-
-print(split_integer(num=int(input("Enter natural number: ")), parts=int(input("Enter how many parts you want already entered number to be splitted in (natural number): "))))
