0c291f8ed64f03b3ee89c56132bfcc15f39a7419
tezeladata <David.tezela@gmail.com>
2025-01-02 01:51:48 +0400

    commit

--- 7kyu.py
+++ 7kyu.py
@@ -1,53 +1,117 @@
-# Array Leaders (Array Series #3)
-def array_leaders(numbers):
-    res = []
-    for i in range(len(numbers)):
-        if numbers[i] > sum(numbers[i+1:]): 
-            res.append(numbers[i])
+# Looking for a benefactor
+def new_avg(arr, newavg):
+    result = round(newavg * (len(arr) + 1) - sum(arr))
+    if result <= 0: raise ValueError("The result must be a positive value.")
+        
+    return result + 1 if (sum(arr) + result) / (len(arr)+1) < newavg else result
+
+# Interactive Dictionary
+class Dictionary():
+    def __init__(self):
+        self.d = {}
+        
+    def newentry(self, word, definition):
+        self.d[word] = definition
+        return self.d
+        
+    def look(self, key):
+        return self.d[key] if key in self.d.keys() else f"Can't find entry for {key}"
+    
+# Check for prime numbers
+def is_prime(n):
+    if n in [0, 1]: return False
+    for i in range(2, int(n*0.5)):
+        if n%i == 0: return False
+    return True
+
+# Previous multiple of three
+def prev_mult_of_three(n):
+    res = str(n)
+    for i in range(len(res)):
+        if int(res) %3 == 0: return int(res)
+        else: res = res[:-1]
+    return None
+
+# Filter Long Words
+def filter_long_words(sentence, n):
+    return [i for i in sentence.split(" ") if len(i) > n]
+
+# Basic Math (Add or Subtract)
+def calculate(s):
+    s = s.replace("plus", "+").replace("minus", "-")
+    tokens = []
+    num = ""
+    for char in s:
+        if char in "+-":
+            if num:
+                tokens.append(num)
+                num = ""
+            tokens.append(char)
+        else:
+            num += char
+    if num:
+        tokens.append(num)
+
+    start = int(tokens[0])
+    for i in range(1, len(tokens), 2):
+        op, num = tokens[i], int(tokens[i + 1])
+        if op == "+":
+            start += num
+        elif op == "-":
+            start -= num
+    return str(start)
+
+# Dictionary from two lists
+def create_dict(keys, values):
+    res = {}
+    for i in range(len(keys)):
+        res[f"{keys[i]}"] = values[i] if i < len(values) else None
     
     return res
 
-# Nth power rules them all!
-def modified_sum(a, n):
-    return sum([i**n for i in a]) - sum(a)
+# Number Pairs
+def get_larger_numbers(a, b):
+    return [a[i] if a[i] > b[i] else b[i] for i in range(len(a))]
 
-# Evens and Odds
-def evens_and_odds(n):
-    return bin(n)[2:] if n%2 == 0 else hex(n)[2:]
+# Float Precision
+def solution(n):
+    return round(n, 2)
 
-# Return the closest number multiple of 10
-def closest_multiple_10(i):
-    return (i // 10)*10 if i%10 < 5 else (i // 10 + 1)*10
-
-# Basic JS - Calculating averages
-class Calculator:
-    @staticmethod
-    def average(*args):
-        return sum(args) / len(args) if len(args) > 0 else 0
+# String Merge!
+def string_merge(string1, string2, letter):
+    start = string1.split(letter)[0] + letter
+    for i in range(len(string2)):
+        if string2[i] == letter:
+            start += string2[i+1:]
+            break
     
-# Selective fear of numbers
-def am_I_afraid(day,num):
-    match day:
-        case "Monday": return False if num != 12 else True
-        case "Tuesday": return False if num <= 95 else True
-        case "Wednesday": return False if num != 34 else True
-        case "Thursday": return False if num != 0 else True
-        case "Friday": return False if num %2 == 1 else True
-        case "Saturday": return False if num != 56 else True
-        case "Sunday": return False if num != 666 and num != -666 else True
+    return start
 
-# Numbers to Letters
-def switcher(arr):
-    alp, res = 'zyxwvutsrqponmlkjihgfedcba!? ', ""
-    return "".join([alp[int(i) - 1] for i in arr])
+# Multiples!
+def multiple(x):
+    if x%3 == 0 and x%5 == 0: return "BangBoom"
+    elif x%3 == 0: return "Bang"
+    elif x%5 == 0: return "Boom"
+    return "Miss"
 
-# Difference Of Squares
-def difference_of_squares(n):
-    return sum(range(n+1))**2 - sum([i**2 for i in range(n+1)])
+# Trimming a string
+def trim(string, max_length):
+    if max_length <= 3:
+        return string[:max_length] + "..." if len(string) > max_length else string
+    return string[:max_length - 3] + "..." if len(string) > max_length else string
 
-# String Reordering
-def sentence(lst):
-    renewed = []
-    for diction in lst:
-        for key, value in diction.items(): renewed.append([int(key), value])
-    return " ".join(i[1] for i in sorted(renewed, key = lambda x: x[0]))
+# last digits of a number
+def solution(n,d):
+    return [int(i) for i in str(n)[-d:]] if d > 0 else []
+
+# Find all occurrences of an element in an array
+def find_all(arr, n):
+    return [i for i in range(len(arr)) if arr[i] == n]
+
+# Find sum of top-left to bottom-right diagonals
+def diagonal_sum(array):
+    return sum(array[i][i] for i in range(len(array)))
+
+# Return a sorted list of objects
+def sort_list(sort_by, lst):
+    return sorted(lst, key=lambda x: x[sort_by], reverse=True)
