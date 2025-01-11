1e9b25bbd3c73a8ae9acd0b45ea8b0eb0ace5e01
tezeladata <David.tezela@gmail.com>
2025-01-12 01:29:03 +0400

    commit

--- string methods/capitalize.py
+++
@@ -1,5 +0,0 @@
-# capitalize(string)
-def capitalize(*args):
-    if not args: raise TypeError("Capitalize takes exactly one argument (0 given)")
-
-    return list(args)[0][0].upper() + list(args)[0][1:]
