65e730ecc0349f58f7e8414b8bac5ff962c309a3
tezeladata <David.tezela@gmail.com>
2025-02-11 01:03:51 +0400

    commit

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { MentorsRoute } from "./Routers/mentors.route.js";
+import { LeadersRoute } from "./Routers/leaders.route.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routers
-app.use("/mentors", MentorsRoute)
+app.use("/mentors", MentorsRoute);
+app.use("/leaders", LeadersRoute);
 
 app.listen(3000, () => console.log("Server started on port 3000"))
---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <section>
+      <h1>Hello</h1>
+    </section>
+  )
+}
