06383bad1756637b84b5c9be20b48673e8c30f85
tezeladata <David.tezela@gmail.com>
2025-03-02 02:23:11 +0400

    commit

--- app.js
+++ app.js
@@ -1,121 +1,98 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+// Server
 const app = express();
 
-// middleware
+// Port
+dotenv.config();
+const PORT = process.env.PORT
+
+// Middleware
 app.use(cors());
-app.use(express.json());
-
-// db
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const filePath = path.resolve(__dirname, './tasks.json');
-
-// utils
-const readDatabase = async () => {
-    try {
-        const data = await fs.promises.readFile(filePath, 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        throw new Error('Failed to read or parse file');
-    }
-};
-const writeDatabase = async (data) => {
-    try {
-        await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
-    } catch (error) {
-        console.error('Error writing to the database:', error);
-        throw new Error('Unable to write to database');
-    }
-};
-
-// Get all tasks
-app.get("/tasks", async (req, res) => {
-    const { limit } = req.query;
-    try {
-        let tasks = await readDatabase();
-
-        if (limit) {
-            tasks = tasks.splice(0, limit);
-        }
-
-        res.json(tasks)
-    } catch(error) {
-        console.log(error)
-    }
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log(`Request url is ${req.url}, request method is ${req.method}`);
+    next();
+});
+app.use((req, res, next) => {
+    console.log(`All headers are: ${req.rawHeaders}`);
+    next();
+})
+app.use((req, res, next) => {
+    console.log(req.errored === null ? "Error not occured": `Error occured ${req.errored}`);
+    next();
 })
 
-// Get single task by ID
-app.get("/tasks/:id", async (req, res) => {
+// Db
+let products = [
+    {id: 1, name: "Apple", price: 1.5},
+    {id: 2, name: "Mango", price: 2.5},
+    {id: 3, name: "Banana", price: 2},
+    {id: 4, name: "Kiwi", price: 4}
+]
+
+// Routes
+app.get("/", (req, res) => {
+    res.json([
+        {name: "David", surname: "Tezelashvili"},
+        {name: "Andria", surname: "Tezelashvili"},
+        {name: "Luka", surname: "Tskhvaradze"},
+        {name: "Vano", surname: "Motiashvili"}
+    ])
+})
+
+app.get("/products", (req, res) => {
+    res.status(200).json(products)
+})
+
+app.post("/products", (req, res) => {
+    const {name, price} = req.body;
+
+    if (!name || !price) {
+        return res.status(404).send("Not enough info")
+    }
+
+    const newProduct = {id: products.length + 1, name, price};
+
+    products.push(newProduct);
+
+    console.log(products)
+    res.status(200).json(products)
+})
+
+app.put("/products", (req, res) => {
+    const {id, name, price} = req.body;
+    const productId = products.findIndex(item => item.id === id && item.name === name);
+
+    if (!id || !name || !price) {
+        return res.status(400).send("Not enough info")
+    }
+
+    products[productId] = {id, name, price};
+
+    if (productId === -1) {
+        return res.status(404).send("Product not found");
+    }
+
+    console.log(products);
+    res.status(200).json(products);
+})
+
+app.delete("/products/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
-    try {
-        const tasks = await readDatabase();
-
-        const result = tasks.filter(task => task.id === id);
-
-        res.json(result)
-    } catch(error) {
-        console.log(error)
+    if (isNaN(id) || id < 1 || id > products.length) {
+        return res.status(400).send("Wrong id")
     }
+
+    products = products.filter(item => item.id !== id);
+
+    console.log(products);
+    res.status(200).json(products);
 })
 
-// Add task
-app.post("/tasks", async (req, res) => {
-    try {
-        let tasks = await readDatabase();
-
-        let newTask = {"id": tasks.length + 1, ...req.body};
-
-        tasks.push(newTask);
-        writeDatabase(tasks);
-
-        res.json(tasks);
-    } catch(e) {
-        console.log(e)
-    }
-})
-
-// Edit task by ID
-app.put("/tasks", async (req, res) => {
-    try {
-        let tasks = await readDatabase();
-
-        const { id, name, duration } = req.body;
-
-        if (!id) {return res.status(400).send("ID not given")};
-
-        const ind = tasks.findIndex(task => task.id === id);
-        if (ind === -1) {return res.status(400).send("Wrong id")};
-        tasks[ind] = {...tasks[ind], name, duration};
-        writeDatabase(tasks)
-
-        res.json(tasks);
-    } catch(e) {
-        console.log(e)
-    }
-});
-
-// Delete task by ID
-app.delete("/tasks", async (req, res) => {
-    const { id } = req.query;
-    try {
-        let tasks = await readDatabase();
-        
-        if (!id) {return res.status(400).send("ID not given")};
-
-        tasks = tasks.filter(task => task.id !== parseInt(id));
-        writeDatabase(tasks);
-
-        res.json(tasks);
-    } catch(e) {
-        console.log(e)
-    }
-})
-
-// Start server
-app.listen(3000, () => console.log("Server started on port 3000"))
+// Run server
+app.listen(PORT, () => console.log(`Server is runnin on port ${PORT}`))
